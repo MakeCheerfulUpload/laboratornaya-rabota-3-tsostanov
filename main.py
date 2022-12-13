022af24c141561b697d830489eda22fa66d7d308
tsostanov <57627648+tsostanov@users.noreply.github.com>
2022-12-13 10:05:32 +0300

    Add files via upload
    доработанная версия

--- main.py
+++ main.py
@@ -1,62 +1,93 @@
-import re
+from time import perf_counter
 
-my_isy_number = 367550
-# 1 часть
-print('Задание 1')
-print('Мой вариант:', my_isy_number % 6, my_isy_number % 4, my_isy_number % 7, '\n')
-my_smile = 'X-{)'
-with open('input.txt', 'r', encoding='utf-8') as f:
-    for current_string in f.readlines():
-        print(f'Количество вхождений искомого смайлика {my_smile} в сообщении:',
-              ''.join(re.findall(r'[^\n]', current_string)), '-', (len(re.findall(r"X-\{\)", current_string))))
 
-# 2 часть
-print('\n' + 'Дополнительное задание 1')
-print('Мой вариант:', my_isy_number % 6, '\n')
-with open('thesis.txt', 'r', encoding='utf-8') as f:
-    for current_thesis in f:
-        default_thesis = current_thesis
-        current_thesis = re.sub(r'[-.,!?<>\s]', ' ', current_thesis)
-        current_thesis = re.sub(r' {2,}', ' ', current_thesis)
-        answer = set()
-        # for length in range(5):
-        #     pattern = ''.join(['\\bВТ\\b(?:[\s\-,]+\w+){', str(length), '}[\s\-,]+\\bИТМО\\b'])
-        #      answer.update(re.findall(pattern, current_thesis))
-        #  answer = sorted(answer, key=len)
-        #  I_REALLY_WANT_TO_STAY_AT_YOUR_HOUSE = ''
-        #  for i in answer:
-        #      I_REALLY_WANT_TO_STAY_AT_YOUR_HOUSE += f'{i};'
-        #  print(f'В сообщении "{default_thesis.strip()}" встречаются следующие подстроки: '
-        #        + I_REALLY_WANT_TO_STAY_AT_YOUR_HOUSE[:-1])
-        # Не учитывает строчки с наложением
-        for index in range(len(re.findall(r'\bВТ\b', current_thesis))):
-            current_thesis = re.sub(r'.*?\bВТ', '-', current_thesis, 1)
-            words = current_thesis.split()
-            delta_thesis = 'ВТ'
-            for i in range(1, min(4, len(current_thesis.split()))):
-                delta_thesis += ' ' + (current_thesis.split())[i]
-                if re.fullmatch(r'.+\bИТМО', delta_thesis):
-                    answer.add(delta_thesis)
-        answer = sorted(answer, key=len)
-        I_REALLY_WANT_TO_STAY_AT_YOUR_HOUSE = ''
-        for i in answer:
-            I_REALLY_WANT_TO_STAY_AT_YOUR_HOUSE += f'{i};'
-        print(f'В сообщении "{default_thesis.strip()}" встречаются следующие подстроки: '
-              + I_REALLY_WANT_TO_STAY_AT_YOUR_HOUSE[:-1])
+def get_information(pattern_information, information):
+    pattern_keys = [i for i in pattern_information.keys()]
+    while '"' in information:
+        delta = information[:information.index('"')]
+        information = information[(information.index('"') + 1):]
+        if delta in pattern_keys:
+            information = information[(information.index('"') + 1):]
+            pattern_information[delta] = information[:information.index('"')]
+    return pattern_information
 
-# 3 часть
-print('\n' + 'Дополнительное задание 2')
-print('Мой вариант:', my_isy_number % 5, '\n')
-with open('emails.txt', 'r', encoding='utf-8') as f:
-    for current_email in f.readlines():
-        current_server = re.findall(r'((?:\w|\.)+@([A-Za-z]+\.[A-Za-z]+))', current_email)
-        try:
-            print('Неверный формат ввода!' if (current_server[0][0] != current_email.strip()) else
-                  f'У почтового адреса {current_email.strip()} почтовый сервер - {current_server[0][1]}')
-        except:
-            print('Неверный формат ввода!')
 
-''' Дополнительное задание с защиты
-string = 'asq <af wqfwg webadgni>Бagea > qqh  pavf GRG'
-print(*re.findall(r'<(.*?)>', string))
-'''
+def about_timetable(information):
+    pattern_information = {'lang': None,
+                           'group': None,
+                           'days': None,
+                           }
+    return get_information(pattern_information, information)
+
+
+def about_lesson(information):
+    pattern_information = {'time': None,
+                           'room': None,
+                           'place': None,
+                           'lesson': None,
+                           'type': None,
+                           'teacher': None
+                           }
+    return get_information(pattern_information, information)
+
+
+def about_days(information):
+    pattern_information = {'name': None,
+                           'lessons': None
+                           }
+    return get_information(pattern_information, information)
+
+
+def get_last_name(current_string):
+    delta_last = current_string.split(',')[-1]
+    return delta_last[1:(delta_last.index(':') - 1)]
+
+
+def writer(nesting_level, key, value, flag):
+    return ' ' * nesting_level + '- ' * flag + ' ' * (not flag) * nesting_level + key + ": " + value
+
+
+def start(input_file, output_file):
+    nesting_level = -1
+    in_array = [[False, 'None'] for size in range(100)]
+    with open(input_file, 'r', encoding='utf-8') as json_file:
+        with open(output_file, 'w', encoding='utf-8') as yaml_file:
+            for current_string in json_file.read().replace('{', '~{').replace('}', '}~').split('~'):
+                nesting_level += current_string.count('{')
+                if '[' in current_string:
+                    in_array[nesting_level] = [True, get_last_name(current_string)]
+                if current_string.count(':') == 1:
+                    in_array[nesting_level][1] = get_last_name(current_string[1:])
+                    current_string = '  ' * (nesting_level - 1) + '- ' * in_array[nesting_level - 1][0] + '  ' * \
+                                     (not in_array[nesting_level - 1][0]) * (nesting_level > 0) \
+                                     + in_array[nesting_level][1] + ':'
+                    yaml_file.write(current_string + '\n')
+                if current_string.count(':') > 1:
+                    flag = in_array[nesting_level - 1][0]
+                    if 'timetable' in in_array[nesting_level - 1][1]:
+                        for key, value in about_timetable(current_string + '""').items():
+                            yaml_file.write(writer(nesting_level, key, value, flag) + '\n')
+                            flag = False
+                    elif 'lesson' in in_array[nesting_level - 1][1]:
+                        for key, value in about_lesson(current_string).items():
+                            yaml_file.write(writer(nesting_level, key, value, flag) + '\n')
+                            flag = False
+                    elif 'days' in in_array[nesting_level - 1][1]:
+                        for key, value in about_days(current_string + '""').items():
+                            yaml_file.write(writer(nesting_level, key, value, flag) + '\n')
+                            flag = False
+
+                if ']' in current_string:
+                    in_array[nesting_level][0] = False
+
+                if '}' in current_string:
+                    nesting_level -= 1
+                    in_array[nesting_level + 1] = [False, 'None']
+
+
+all_time = 0
+for _ in range(100):
+    start_time = perf_counter()
+    start('timetable.json', 'timetable.yaml')
+    all_time += perf_counter() - start_time
+print(all_time)
